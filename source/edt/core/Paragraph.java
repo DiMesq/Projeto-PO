691e72585bbbe7d4d691a4163e63fd0bee02ce2e
DReigada <daniel.reigada@gmail.com>
2015-11-04 11:14:43 +0000

    Add the Paragraph class.

---
+++ source/edt/core/Paragraph.java
@@ -0,0 +1,44 @@
+package edt.core;
+
+/**
+ * This class implements a Paragraph of a {@link Section}.
+ * <p>A Paragraph has some content represented as a String
+ * 
+ * @author Daniel Reigada
+ * @author Diogo Mesquita
+ * @author Sebastião Araújo
+ * @version 1.0
+*/
+public class Paragraph extends TextElement {
+	/**
+	 * The Content of the Paragraph
+	 */
+	private String _text;
+
+	/**
+	 * Sets/changes the content of this Paragraph
+	 *
+	 * @param text The new content to be set
+	 */
+	public void setText(String text){
+		_text = text;
+	}
+
+	/**
+	 * Returns the content of this Paragraph
+	 *
+	 * @return The content of this Paragraph
+	*/
+	public String getContent(){
+		return _text;
+	}
+
+	/**
+	 * Returns the size of this Paragraph
+	 *
+	 * @return The size of this Paragraph
+	 */
+	public int getSize(){
+		return _text.length();
+	}
+}
