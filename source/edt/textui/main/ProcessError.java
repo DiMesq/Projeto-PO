fedfccddf495e41fff77d65ee12cb01fabc49121
DiMesq <d.mesquita285@gmail.com>
2015-11-16 12:38:44 +0000

    fix: wasnt taking care of one case.

--- source/edt/textui/main/ProcessError.java
+++ source/edt/textui/main/ProcessError.java
@@ -6,23 +6,28 @@ import pt.utl.ist.po.ui.Display;
 
 public class ProcessError{
 
-	public static void processError(TextElementException exception, String value){
+	public static void processError(TextElementException exception, String name){
 
 		Display display = new Display();
 
 		switch (exception.getErrorCode()){
 			case "DOCUMENT_NOT_FOUND":
-				display.addNewLine(Message.fileNotFound(value));
+				display.addNewLine(Message.fileNotFound(name));
 
 			case "DOCUMENT_DESERIALIZE_EXCEPTION":
-				display.addNewLine(Message.fileNotFound(value));
+				display.addNewLine(Message.fileNotFound(name));
 
 			case "DOCUMENT_SERIALIZE_EXCEPTION":
-				display.addNewLine(Message.fileNotFound(value));
+				display.addNewLine(Message.fileNotFound(name));
+
+			case "FILE_CLOSE_EXCEPTION":
+				display.addNewLine(Message.fileNotFound(name));
 
 			default:
 				System.err.println("Unknown exception occurred: " + exception.getMessage());
 				exception.printStackTrace();
 		}
+
+		display.display();
 	}
 }
