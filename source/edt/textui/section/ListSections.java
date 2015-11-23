c9df3aa09c42b89336a0cc9ddde7e9518bb7665f
DiMesq <d.mesquita285@gmail.com>
2015-11-23 11:58:50 +0000

    Modify the List sections command to use the visitor design pattern.

--- source/edt/textui/section/ListSections.java
+++ source/edt/textui/section/ListSections.java
@@ -1,13 +1,11 @@
 package edt.textui.section;
 
 import edt.core.Section;
-import edt.core.Paragraph;
 
 import pt.utl.ist.po.ui.Display;
 import pt.utl.ist.po.ui.Command;
-import pt.utl.ist.po.ui.Display;
 
-import java.util.List;
+import edt.textui.visitors.ListSectionsVisitor;
 
 /**
  * Command for listing all subsections of the current section.
@@ -23,41 +21,14 @@ public class ListSections extends Command<Section> {
         super(MenuEntry.LIST_SECTIONS, ent);
     }
 
-    //TODO: mudar isto tudo usando o padrão de desenho VISITOR
-    /**
-     * Returns the titles from every subsection of the current one
-     *
-     * @param Section the section to get the subsection's titles from 
-     * @param boolean first_call indicates if a call to this function is the
-     * first one (true) or a recursive one (false).
-     *
-     * @return String all titles from the subsections 
-     */
-    private String getSections(Section section, boolean firstCall){
-
-        String content = Message.sectionIndexEntry(section.getKey(), 
-                                                   section.getTitle()) + "\n";
-
-        if (firstCall) content = "";
-                    
-        List<Paragraph> paragraphs = section.getParagraphs();
-        List<Section> subSections = section.getSubsections();
-
-        for (Section s: subSections) content += this.getSections(s, false);
-
-        return content;
-    }
     /**
      * Execute the command.
      */
     @Override
     @SuppressWarnings("nls")
     public final void execute() {
-        Display display = new Display();
 
-        //show the current section subsection's titles
-        display.add(this.getSections(entity(), true));
+        entity().accept( new ListSectionsVisitor());
 
-        display.display();
     }
 }
