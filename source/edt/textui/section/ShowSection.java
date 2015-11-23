f5bd0d17d68d4ba9aa8ec2f274ffaf69719591f4
DiMesq <d.mesquita285@gmail.com>
2015-11-23 11:56:52 +0000

    Modify the ShowSection command to use the Visitor design pattern.

--- source/edt/textui/section/ShowSection.java
+++ source/edt/textui/section/ShowSection.java
@@ -1,13 +1,11 @@
 package edt.textui.section;
 
 import edt.core.Section;
-import edt.core.Paragraph;
 
-import pt.utl.ist.po.ui.Display;
+import edt.textui.visitors.GetContentVisitor;
+
 import pt.utl.ist.po.ui.Command;
-import pt.utl.ist.po.ui.Display;
 
-import java.util.List;
 /**
  * Command for showing the content of current section.
  */
@@ -22,41 +20,14 @@ public class ShowSection extends Command<Section> {
         super(MenuEntry.SHOW_CONTENT, ent);
     }
 
-    //TODO: mudar isto tudo usando o padrão de desenho VISITOR
-    /**
-     * Returns all of the current section's content
-     * 
-     * @param Section the section to get all content from 
-     *
-     * @return String all of the content from every subsection of the current one
-     */
-    private String getSections(Section section){
-
-        String content = Message.sectionIndexEntry(section.getKey(), 
-                                                   section.getTitle()) + 
-                                                   "\n";
-
-        List<Paragraph> paragraphs = section.getParagraphs();
-        List<Section> subSections = section.getSubsections();
-
-        for (Paragraph p: paragraphs) content += p.getContent() + "\n";
-
-        for (Section s: subSections) content += this.getSections(s);
-
-        return content;
-    }
-
     /**
      * Execute the command.
      */
     @Override
     @SuppressWarnings("nls")
     public final void execute() {
-        Display display = new Display();
 
-        //show all content from the current section and all its subsections
-        display.add(this.getSections(entity())); 
+        entity().accept( new GetContentVisitor());
 
-        display.display();
      }
 }
