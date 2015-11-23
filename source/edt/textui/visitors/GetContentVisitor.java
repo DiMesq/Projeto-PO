f76f83b7abcf81bb97b41a055bd333a6f83cef02
DiMesq <d.mesquita285@gmail.com>
2015-11-23 11:04:13 +0000

    Add the get content operation using a visitor.

---
+++ source/edt/textui/visitors/GetContentVisitor.java
@@ -0,0 +1,78 @@
+package edt.textui.visitors;
+
+import edt.visitor.Visitor;
+
+import edt.core.Paragraph;
+import edt.core.Section;
+
+import edt.textui.section.Message;
+
+import pt.utl.ist.po.ui.Display;
+
+import java.util.List;
+
+/**
+ * GetContentVisitor implements the Visitor interface.
+ * <p>GetContentVisitor performs the getContent operation on sections and paragraphs.
+ * 
+ * @author Daniel Reigada
+ * @author Diogo Mesquita
+ * @author Sebastião Araújo
+ * @version 1.0
+*/
+public class GetContentVisitor implements Visitor {
+
+	/**
+	 * The Visitor's visit method implementation for Paragraph Element.
+	 *
+	 * @param Paragraph p - the element to visit
+	 * @Override
+	 */
+	public void visit(Paragraph p) {
+
+		Display display = new Display();
+
+		display.add(p.getContent())
+			   .display();
+
+	}
+
+	/**
+	 * The Visitor's visit method implementation for Section Element.
+	 *
+	 * @param Section s - the element to visit
+	 * @Override
+	 */
+	public void visit(Section s) {
+
+		Display display = new Display();
+
+		display.add(getSections(s))
+			   .display();
+	}
+
+	/**
+     * Returns all of the Section section content
+     * 
+     * @param Section the section to get all content from 
+     *
+     * @return String all of the content from every subsection of the current one
+     */
+    private String getSections(Section section){
+
+        String content = Message.sectionIndexEntry(section.getKey(), 
+                                                   section.getTitle()) + 
+                                                   "\n";
+
+        List<Paragraph> paragraphs = section.getParagraphs();
+        List<Section> subSections = section.getSubsections();
+
+        for (Paragraph p: paragraphs) content += p.getContent() + "\n";
+
+        for (Section s: subSections) content += this.getSections(s);
+
+        return content;
+    }
+}
+
+
