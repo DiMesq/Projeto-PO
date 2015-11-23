1e7ede00695577ab30233cd0a3c2613d69d0ef20
DiMesq <d.mesquita285@gmail.com>
2015-11-23 11:56:08 +0000

    Modify the ShowTextElement command to use the visitor design.

--- source/edt/textui/main/ShowTextElement.java
+++ source/edt/textui/main/ShowTextElement.java
@@ -2,12 +2,12 @@ package edt.textui.main;
 
 import edt.core.*;
 
+import edt.textui.visitors.GetContentVisitor;
+
 import pt.utl.ist.po.ui.Command;
 import pt.utl.ist.po.ui.Display;
 import pt.utl.ist.po.ui.Form;
 import pt.utl.ist.po.ui.InputString;
-
-import java.util.List;
 
 /**
  * Command for showing the text element with a given identifier of the current document in the editor.
@@ -23,30 +23,7 @@ public class ShowTextElement extends Command<DocManager> {
         super(MenuEntry.SHOW_TEXT_ELEMENT, ent);
     }
 
-    //TODO: mudar isto tudo usando o padrão de desenho VISITOR
-    /**
-     * Returns all of the current section's content
-     * 
-     * @param Section the section to get all content from 
-     *
-     * @return String all of the content from every subsection of the current one
-     */
-    private String getSections(Section section){
-
-        String content = Message.sectionIndexEntry(section.getKey(), 
-                                                   section.getTitle()) + 
-                                                   "\n";
-
-        List<Paragraph> paragraphs = section.getParagraphs();
-        List<Section> subSections = section.getSubsections();
-
-        for (Paragraph p: paragraphs) content += p.getContent() + "\n";
-
-        for (Section s: subSections) content += this.getSections(s);
-
-        return content;
-    }
-
+    //TODO: mudar isto tudo - arranjar solucao melhor que as excecoes!!!
     /**
      * Execute the command.
      */
@@ -60,15 +37,13 @@ public class ShowTextElement extends Command<DocManager> {
         InputString in = new InputString(form, Message.requestElementId());
         form.parse();
 
-        // TODO: para não ser preciso esta porcaria tambem precisamos do VISITOR: no textElement 
-        // tem entao de estar definido o metodo getContent em abstract
         try{
             Section section = (Section) entity().getDocument().getTextElement(in.value());
 
             if (section == null) display.addNewLine(Message.noSuchTextElement(in.value()))
                                     .display();
 
-            else display.addNewLine(getSections(section)).display();
+            else section.accept( new GetContentVisitor());
 
         } catch (ClassCastException c){
             Paragraph paragraph = (Paragraph) entity().getDocument()
